462f89f72eadc10559a41c7cd32d4dd14a26d31c
David <nedelcudavid1@gmail.com>
2025-03-04 11:34:51 +0200

    modificare2

--- lab2.c
+++ lab2.c
@@ -2,19 +2,20 @@
 #include <stdio.h>
 
 int function(int x){
+    if(x<=0)return x;
 	x=x-2;
-	x=function(x);
+	///x=function(x);
 	return x;
 }
 
 int main(){
 	int a=30, i=0;
-	
+
 	while (a>0){
 		a=function(a);
 		i++;
 	}
 	printf("%d", i);
-	return 0;	
+	return 0;
 }
 
